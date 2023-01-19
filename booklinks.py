8c84effbf1f902fa1568691b311a3ab6670ce1ea
= <=>
2023-01-19 10:48:46 +0530

    More books Added

---
+++ booklinks.py
@@ -0,0 +1,67 @@
+book_code={
+    "LA1" :"https://drive.google.com/file/d/1VQIAA8g-C9Ynwq8MubUwskm8uEWY5qQf/view?usp=share_link",
+
+    "BD1" :"https://drive.google.com/file/d/19xcnFVcxlnycy7-OltYwy6OhbdxS1xpp/view?usp=share_link",
+    "BD2" :"https://drive.google.com/file/d/1airyVDFuD11aLLGDVWPPJIFQeivmkAad/view?usp=share_link",
+
+    "CC1" :"https://drive.google.com/file/d/1BcauSjks6m2BX3CMeV8B5eH_i2781gr1/view?usp=share_link",
+    "CC2" : "https://drive.google.com/file/d/1js4D3YZnb8Fete7eNPvSA2DKV_5EJ7eG/view?usp=share_link",
+
+    "DB1" :"https://drive.google.com/file/d/1nWFoFedNlu6t3qrfSPDmDLDwlmNgKStu/view?usp=share_link",
+    "DB2" :"https://drive.google.com/file/d/1W7fDTLPrqz9BIeFCWtrdAzSSpE6RLk9C/view?usp=share_link",
+    "DB3" :"https://drive.google.com/file/d/1cPXplUyXcjKz0yZothNCp5jghwXVlXBy/view?usp=share_link",
+    "DB4" :"https://drive.google.com/file/d/1ol3IB-Jh08sr6DV_St-idFweEZ9stXdp/view?usp=share_link",
+    "DB5" :"https://drive.google.com/file/d/1aDP_yrCZA_tj1B1BrLBjRjxO2tOpmwPX/view?usp=share_link",
+    "DB6" :"https://drive.google.com/file/d/1J_HWAVydZxdOty9S9AgG-MTC6LW_MdBi/view?usp=share_link",
+
+    "CV1" :"https://drive.google.com/file/d/1hxAtVNLu6GiSOTB1yfLaeKjcOw-ilTdE/view?usp=share_link",
+
+    "DS1" :"https://drive.google.com/file/d/16KsXPNd5VhP6GXr_1bCXjWXPfXPmem2Q/view?usp=share_link",
+
+    "JA1"  :"https://drive.google.com/file/d/1B0JzqwUou_q8mbPXcZO_9BScaWTBbCWK/view?usp=share_link",
+
+    "KR1"  :"https://drive.google.com/file/d/1B0JzqwUou_q8mbPXcZO_9BScaWTBbCWK/view?usp=share_link",
+
+    "ML1" :"https://drive.google.com/file/d/1aBXB8N0coSZtW_sloklrWVaU5agIBLqH/view?usp=share_link",
+    "ML2" :"https://drive.google.com/file/d/17d57PQj3fRhJjBvdj9PRPST4YtMZZ6w-/view?usp=share_link",
+
+    "MP1" :"https://drive.google.com/file/d/1VAXtIYufhOg9kCXVCL_vSpl_z3v9DUPf/view?usp=share_link",
+    "MP2" :"https://drive.google.com/file/d/1kurHad5rNx26KrNnH2WWBIbjg_A2PhNn/view?usp=share_link",
+    "MP3" :"https://drive.google.com/file/d/108VamWnL3znstKc2mv1Z0mmEpdAYWPTd/view?usp=share_link",
+
+    "RL1" :"https://drive.google.com/file/d/18DcM7CNUSkPvLF9Ej-Oe3gKlL0ZYK9Fn/view?usp=share_link",
+    "RL2" :"https://drive.google.com/file/d/1goJaYtUcYcn3WD0vZfQaiuygOmeQHAVL/view?usp=share_link",
+
+    "SP1" :"https://drive.google.com/file/d/1DRgojY0OSy7MaKvYNxxMVglX9uAsa3Bg/view?usp=share_link",
+    "SP2" :"https://drive.google.com/file/d/16teq6auYlcza3AGYze0w-qtiIHB08ASK/view?usp=share_link",
+    "SP3" :'https://drive.google.com/file/d/1NVHeMRmH_dB7dngVgblBU3sftQLi4V8g/view?usp=share_link',
+
+    "PR1" :"https://drive.google.com/file/d/1Xh1Id1ixOAygFhdgkwYA3Sm6nG14H9YF/view?usp=share_link",
+    "PR2" :"https://drive.google.com/file/d/1Xh1Id1ixOAygFhdgkwYA3Sm6nG14H9YF/view?usp=share_link",
+    "PR3" :"https://drive.google.com/file/d/1Qwx4gMwvz--3FD94Ih-cPLgb8CNUYUzV/view?usp=share_link",
+    "PR4" :"https://drive.google.com/file/d/131BpUJnif5cCofHKRebxzChA9t5eSEl2/view?usp=share_link",
+    "PR5" :"https://drive.google.com/file/d/1a--7tS7UJ-C-aNvYyohr6XZc40k48_BL/view?usp=share_link",
+    "PR6" :"https://drive.google.com/file/d/1xQhhpr56xexIrY4eO1gH1PThJQ7JkqMm/view?usp=share_link",
+    "PR7" :"https://drive.google.com/file/d/1yipVtSl9QQRaM34n75hq0Z5YtAL_HGa-/view?usp=share_link",
+    "PR8" :"https://drive.google.com/file/d/1PNyvMoUwFxFPEmFWQW2AUIUhTQ5A0Q_G/view?usp=share_link",
+
+
+    "SSP1":"https://drive.google.com/file/d/1uOGFsF57qtxZQesh88xWyg6q1yJLKrm6/view?usp=share_link",
+    "SSP2":"https://drive.google.com/file/d/13sPVRopLeViABWWPPKbN_YkFpBPy9t-g/view?usp=share_link",
+    "SSP3":'https://drive.google.com/file/d/1pRjIvTKno6xyu4wsT5DC63cGO7L3HiPX/view?usp=share_link',
+    "SSP4":"https://drive.google.com/file/d/1gIHb501N2p1-W9lmyzMKYOdFKabxQZov/view?usp=share_link",
+   
+
+
+    "PY1" :"https://drive.google.com/file/d/15mjmS1pKJxZivoCBPJWbWBcStIZz_ZKE/view?usp=share_link",
+    "PY2" :"https://drive.google.com/file/d/13yq-QhCXpMVkOfg8_oWOK8EJ5u1X5MZP/view?usp=share_link",
+    "PY3" :"https://drive.google.com/file/d/1vdl8Ln-E7XX_R5PpeyAO5WtYAX2y5G1Y/view?usp=share_link",
+    "PY4" :"https://drive.google.com/file/d/14m1LTEBudjBrVji3x2CmsU4JJYYKWWv6/view?usp=share_link",
+
+
+
+    "PP1" :"https://drive.google.com/file/d/1Te1owVAFIPyrEZuZwI_V4k_1MXeF-T6f/view?usp=share_link",
+
+    "StatSP1":"https://drive.google.com/file/d/1IPcKFHjbTgVxMZFcx9X9UA-DyuCREYPd/view?usp=share_link",
+}
+            
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import telegram.ext
 from requests import *
 from telegram import *
+import booklinks as bl
+import re
 
 token = '5738509972:AAEdL_Z2LvEepQfk3E9TxZNk1AkG5sF3sGY'
 updater = telegram.ext.Updater(token=token, use_context=True)
@@ -8,47 +10,157 @@ dispatcher = updater.dispatcher
 
 
 
-
-def startCommand(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, I'm SCAR .I can help you with your Academic Documents\nType /options to see what I can do.")
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Here are the list of services")
-
-def options(update, context):
-        context.bot.send_message(chat_id=update.effective_chat.id, text="/cho - To get the course handouts for any subject")
+def Start(update,context):
+    re.split("[^a-zA-Z]*", str(update.message.from_user.first_name))
+    name = "".join(re.split("[^a-zA-Z]*", str(update.message.from_user.first_name)))
+    context.bot.send_message(chat_id=update.effective_chat.id, text= f"Hey {name}, Let's get started! Use the Menu button below to see what I am capable of")
 
 
 
-def cho(update, context):
-        context.bot.send_message(chat_id=update.effective_chat.id, text="The course Handouts for you are as follows");
-        course_hand_out_options = []
-        count=1
-        with open('Subjects.txt') as f:
-            for line in f:
-                inline = [InlineKeyboardButton(line.rstrip(), callback_data=str(count))]
-                course_hand_out_options.append(inline)
-                count = count + 1
+def timetable(update,context): 
+    year = update.message.from_user.first_name[0:2]
+    section_code = update.message.from_user.first_name[4:6]
+
+    if year=="21":
+        if section_code=="08":
+            context.bot.send_document(chat_id=update.effective_chat.id, document=open(f"Timetables/Y{year}/Y{year}-AIDS.jpg", 'rb'))
+
+
+        elif section_code=="04":
+            ece_sections=[]
+            count=1
+            with open('ECE.txt') as f:
+                for line in f:
+                    inline = [InlineKeyboardButton(line.rstrip(), callback_data=f"{year[1:]} {line.rstrip()}")]
+                    ece_sections.append(inline)
+                    count=count+1
+    
+            reply_markup = InlineKeyboardMarkup(ece_sections)
+            context.bot.send_message(chat_id=update.effective_chat.id, text="Which section do you belong to?", reply_markup=reply_markup)
+
+        elif section_code=="03":
+            cse_sections=[]
+            count=1
+            with open('CSE.txt') as f:
+                for line in f:
+                    inline = [InlineKeyboardButton(line.rstrip(), callback_data=f"{year[1:]} {line.rstrip()}")]
+                    cse_sections.append(inline)
+                    count=count+1
+
+            reply_markup = InlineKeyboardMarkup(cse_sections)
+            context.bot.send_message(chat_id=update.effective_chat.id, text="Which section do you belong to?", reply_markup=reply_markup)
+        else :
+            pass
+
+
+
+    elif year=="22":
+            if section_code=="08":
+                context.bot.send_document(chat_id=update.effective_chat.id, document=open(f"Timetables/Y{year}/Y{year}-AIDS.jpg", 'rb'))
+            elif section_code=="04":
+                ece_sections=[]
+                count=1
+                with open('ECE.txt') as f:
+                    for line in f:
+                        inline = [InlineKeyboardButton(line.rstrip(), callback_data=f"{year[1:]} {line.rstrip()}")]
+                        ece_sections.append(inline)
+                        count=count+1
+    
+                reply_markup = InlineKeyboardMarkup(ece_sections)
+                context.bot.send_message(chat_id=update.effective_chat.id, text="Which section do you belong to?", reply_markup=reply_markup)
+    
+            elif section_code=="03":
+                cse_sections=[]
+                count=1
+                with open('CSE.txt') as f:
+                    for line in f:
+                        inline = [InlineKeyboardButton(line.rstrip(), callback_data=f"{year[1:]} {line.rstrip()}")]
+                        cse_sections.append(inline)
+                        count=count+1
+                reply_markup = InlineKeyboardMarkup(cse_sections)
+                context.bot.send_message(chat_id=update.effective_chat.id, text="Which section do you belong to?", reply_markup=reply_markup)
  
-        reply_markup = InlineKeyboardMarkup(course_hand_out_options)
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Choose an option", reply_markup=reply_markup)
+
+
+
+def library(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the Library\nHere you can find the books you need\nChoose a subject below to explore more")
+    library_options = []
+    count=0
+    with open('Library.txt') as f:
+        for line in f:
+            inline = [InlineKeyboardButton(line.rstrip(), callback_data=subject_codes[count])]
+            library_options.append(inline)
+            count = count+1
+    reply_markup = InlineKeyboardMarkup(library_options)
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Choose an option", reply_markup=reply_markup)
+
+        
+def showBooks(update,context,subject):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="The books for you are as follows")
+    books = []
+    count = 1
+    with open(f'Books/{subject}.txt') as f:
+        for line in f:
+            inline = [InlineKeyboardButton(line.rstrip(), callback_data=f'{subject}'+str(count))]
+            books.append(inline)
+            count = count+1
+    reply_markup = InlineKeyboardMarkup(books)
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Choose an option", reply_markup=reply_markup)
 
 def query_handler(update, context):
 
     query = update.callback_query.data
-    if query == "1":
-        context.bot.send_document(chat_id=update.effective_chat.id, document=open("AI_CHO.pdf", 'rb'))
-    elif query == "2":
-        context.bot.send_document(chat_id=update.effective_chat.id, document=open("DM_CHO.pdf", 'rb'))
-    elif query == "3":
-        context.bot.send_document(chat_id=update.effective_chat.id, document=open("JAVA_CHO.pdf", 'rb'))
-    elif query == "4":
-        context.bot.send_document(chat_id=update.effective_chat.id, document=open("PS_CHO.pdf", 'rb'))
-    elif query == "5":
-        context.bot.send_document(chat_id=update.effective_chat.id, document=open("OS_CHO.pdf", 'rb'))
+    if query in subject_codes:
+        showBooks(update,context,query)
+    elif query in bl.book_code.keys():
+        context.bot.send_message(chat_id=update.effective_chat.id, text = bl.book_code[query])
+
+    year = "Y"+query.split()[0]
+    department = query.split()[1]
+    section = query.split()[2]
+    if department in ["CSE","ECE"]:
+        context.bot.send_document(chat_id=update.effective_chat.id, document=open(f"Timetables/{year}/{year}-{department}-{section}.jpg", 'rb'))
+    
+    
 
 
-dispatcher.add_handler(telegram.ext.CommandHandler('start', startCommand))
-dispatcher.add_handler(telegram.ext.CommandHandler('cho', cho))
-dispatcher.add_handler(telegram.ext.CommandHandler('Options', options))
+
+
+
+subjects={
+"Linear Algebra" : "LA",
+"Artificial Intelligence": "AI",
+"Big Data" : "BD",
+"Cloud Computing" : "CC",
+"Computer Vision" : "CV",
+"Data Structures" : "DS",
+"Java" : "JA",
+"Knowledge Representation" : "KR",
+"Machine Learning" : "ML",
+"Mathematical Programming":  "MP",
+"Parallel Processing CUDA" : "PP",
+"Probability"  : "PR",
+"Python" : "PY",
+"Reinforcement Learning" : "RL",
+"Signal Processing" : "SP",
+"Speech Signal Processing" : "SSP",
+"Statistical Signal Processing" : "StatSP",
+}
+subject_codes = [x for x in subjects.values()]
+
+
+
+
+    
+       
+
+
+
+        
+dispatcher.add_handler(telegram.ext.CommandHandler('start',Start))
+dispatcher.add_handler(telegram.ext.CommandHandler('library', library))
+dispatcher.add_handler(telegram.ext.CommandHandler('timetable', timetable))
 
 dispatcher.add_handler(telegram.ext.CallbackQueryHandler(query_handler))
 updater.start_polling()
